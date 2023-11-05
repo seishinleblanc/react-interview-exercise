72a9a0eb38ec444c3b71334c62fed3917bcc861d
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-11-05 16:00:46 -0600

    Added toggle feature allowing 'search by districts' or 'search by schools'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Header from '@components/Header'
 import Home from '@components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/react-interview-exercise' element={ <Home /> } />
+      </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
